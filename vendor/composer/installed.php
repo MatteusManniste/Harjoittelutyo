789ab307b175e01dcbd570ad95780c760eff679f
Matteus Männiste <matteus.manniste@gmail.com>
2022-12-09 15:33:13 +0200

    Added autoloading and setup MVC folder structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '714cf613e2682509d8d8e9d0ddbaa68c037c21e9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '714cf613e2682509d8d8e9d0ddbaa68c037c21e9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
