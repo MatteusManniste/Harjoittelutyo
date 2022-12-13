124ab8705a6cd25ddbbb73718508f91ad69dca7e
Matteus Männiste <matteus.manniste@gmail.com>
2022-12-13 14:49:31 +0200

    Added login (empty)

---
+++ app/views/pages/login.php
@@ -0,0 +1,12 @@
+<?php require_once "../app/views/inc/header.php" ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php require_once "../app/views/inc/navbar.php" ?>
+    <h1>Login</h1> 
+    <?php require_once "../app/views/inc/footer.php" ?>
+</body>
+
+</html>
