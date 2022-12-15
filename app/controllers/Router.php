7b4e10639a571fe2e4ee0ea610efca6acdb70084
Matteus Männiste <matteus.manniste@gmail.com>
2022-12-15 19:17:10 +0200

    Improved and removed some unnecessary stuff

--- app/controllers/Router.php
+++ app/controllers/Router.php
@@ -6,9 +6,10 @@ class Router
 {
     private function getUrl()
     {
-        if (isset($_GET["url"])) {
-            $url = $_GET["url"];
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
             return $url;
         }
     }
@@ -17,15 +18,12 @@ class Router
     {
         $url = $this->getUrl();
 
-        if (isset($url) && file_exists(__DIR__ . "/../views/pages/" . $url . ".php")) {
-            require_once __DIR__ . "/../views/pages/" . $url . ".php";
-            exit;
-        } elseif (isset($url) && !file_exists(__DIR__ . "/../views/pages/" . $url . ".php")) {
+        if (isset($url) && file_exists(__DIR__ . "/../views/pages/" . $url[0] . ".php")) {
+            require_once __DIR__ . "/../views/pages/" . $url[0] . ".php";
+        } elseif (isset($url) && !file_exists(__DIR__ . "/../views/pages/" . $url[0] . ".php")) {
             require_once __DIR__ . "/../views/errors/_404.php";
-            exit;
         } else {
             require_once __DIR__ . "/../views/pages/index.php";
-            exit;
         }
     }
 }
